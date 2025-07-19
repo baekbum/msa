0f2aa66b29da284ee2769bbd5051d133f084e0df
baekbum <baekbum828@naver.com>
2025-07-19 21:16:41 +0900

    user-service에 spring-security 기능 추가

--- user-service/src/main/java/com/example/user_service/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/user_service/service/UserServiceImpl.java
@@ -3,28 +3,27 @@ package com.example.user_service.service;
 import com.example.user_service.dto.UserDto;
 import com.example.user_service.jpa.UserEntity;
 import com.example.user_service.jpa.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    Environment env;
-
-    UserRepository userRepository;
-
-    public UserServiceImpl(Environment env, UserRepository userRepository) {
-        this.env = env;
-        this.userRepository = userRepository;
-    }
+    private final Environment env;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
         UserEntity userEntity = new UserEntity(userDto);
-        userEntity.setEncryptedPwd("encrypted_password");
+        //userEntity.setEncryptedPwd("encrypted_password");
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 
         userRepository.save(userEntity);
 
