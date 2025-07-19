8e510993d0fbab19b912ed54afa9c25157d65cba
baekbum <baekbum828@naver.com>
2025-07-19 19:53:52 +0900

    api gateway (게이트 웨이) 추가

--- apigateway-service/src/main/java/com/example/apigateway_service/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigateway_service/ApigatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.user_service;
+package com.example.apigateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
-public class UserServiceApplication {
+public class ApigatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(ApigatewayServiceApplication.class, args);
 	}
 
 }
