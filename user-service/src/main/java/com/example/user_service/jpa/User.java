b2440b868279e7323300e21b0f32a53ca4632dbf
baekbum <baekbum828@naver.com>
2025-08-06 18:45:15 +0900

    user -> order feignClient 사용시 circuitBreaker 적용

--- user-service/src/main/java/com/example/user_service/jpa/User.java
+++ user-service/src/main/java/com/example/user_service/jpa/User.java
@@ -4,6 +4,9 @@ import com.example.user_service.dto.UserDto;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -14,19 +17,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 50, unique = true)
-    private String email;
-    @Column(nullable = false, length = 50)
-    private String name;
     @Column(nullable = false, unique = true)
     private String userId;
+
     @Column(nullable = false)
-    private String encryptedPwd;
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false) // updatable = false 설정으로 업데이트 시 시간 변경 방지
+    private LocalDateTime createAt;
 
     public User(UserDto dto) {
-        this.email = dto.getEmail();
-        this.name = dto.getName();
         this.userId = dto.getUserId();
+        this.name = dto.getName();
+        this.email = dto.getEmail();
     }
-
 }
